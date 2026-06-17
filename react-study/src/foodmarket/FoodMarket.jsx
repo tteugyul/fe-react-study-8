d8e25249911d2611fe50941e867c57a0de8b3a7e
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-06-17 12:19:43 +0900

    FoodCard 컴포넌트화, foodsData 이미지 경로 추가, 이미지 접근 시 public 경로 접근, 상세경로 설정 시 접근 경로

--- react-study/src/foodmarket/FoodMarket.jsx
+++ react-study/src/foodmarket/FoodMarket.jsx
@@ -1,4 +1,9 @@
 import './FoodMarket.css';
+import FoodCard from './components/FoodCard';
+
+// 화면에 보여주는 푸드 정보 기준 data import
+import foodsData from './data/foodsData';
+
 // import Button from 'react-bootstrap/Button';
 // import Spinner from 'react-bootstrap/Spinner';
 // import Container from 'react-bootstrap/Container';
@@ -10,7 +15,11 @@ import food1 from './img/food1.jpg';
 import food2 from './img/food2.jpg';
 import food3 from './img/food3.jpg';
 
+import { useState } from 'react';
+
 function FoodMarket() {
+    let [foods, setFoods] = useState(foodsData);
+
     return (
         <div>
             <Navbar bg="light" data-bs-theme="light">
@@ -27,6 +36,32 @@ function FoodMarket() {
             {/*
                 이미지 사용
 
+                1. css 에서 사용
+                    이미지 경로 (상대경로 ./img/...)
+                
+                2. js(jsx) 에서 사용
+                    상대경로 연결 사용 XXX
+                    <img src="../../image/menu.png"/>
+
+                    react 컴포넌트에서 이미지 사용시, import -> 사용
+                    <img src="https://...외부url"/>
+                    <img src={import한 이미지}/>
+
+                3. public 폴더에 이미지를 미리 저장해놓고 사용
+                    현재 실행중인 서버가 public 경로를 통해 image 에 접근할 수 있는 서버 역할
+                    이미지 외부 url에 접근해서 사용하듯이 ...사용
+
+                    <img src={'http://localhost:5173/images/food1.jpg}/>
+                    <img src={'/images/food1.jpg'}/>
+
+                    CRA     :   <img src={process.env.PUBLIC_URL + '/images/food1.jpg'}/>
+                                package.json
+                                "homepage":"/detailServicePath" 상세경로설정
+
+                    Vite    :   <img src={import.meta.env.BASE_URL + '/images/food1.jpg'}/>
+                                vite.config.js
+                                base: "/detailServicePath/" 상세경로설정
+
                 상대경로 연결 사용 XXX
                 <img src="../../image/menu.png"/> 작동 X
 
@@ -41,15 +76,27 @@ function FoodMarket() {
 
             <Container>
                 <Row>
+                    {
+                        foods.map((food, index) => {
+                            return (
+                                <Col md={4} sm={2}>
+                                    <FoodCard food={food} foods={foods} index={index} />
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
+            </Container>
+            {/* <Container>
+                <Row>
                     <Col md={4} sm={2}>
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={food1} />
                             <Card.Body>
-                                <Card.Title>카드 타이틀</Card.Title>
-                                <Card.Text>
-                                    내부 내용쓰는 곳. p 태그 인식
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Title>{foods[0].title}</Card.Title>
+                                <Card.Text>{foods[0].content}</Card.Text>
+                                <Card.Text>{foods[0].price}</Card.Text>
+                                <Button variant="primary">상세보기</Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -57,11 +104,10 @@ function FoodMarket() {
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={food2} />
                             <Card.Body>
-                                <Card.Title>감자튀김</Card.Title>
-                                <Card.Text>
-                                    아주 맛있는 맥도날드 감자튀김
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Title>{foods[1].title}</Card.Title>
+                                <Card.Text>{foods[1].content}</Card.Text>
+                                <Card.Text>{foods[1].price}</Card.Text>
+                                <Button variant="primary">상세보기</Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -69,16 +115,15 @@ function FoodMarket() {
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={food3} />
                             <Card.Body>
-                                <Card.Title>햄버거</Card.Title>
-                                <Card.Text>
-                                    평범한 햄버거
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Title>{foods[2].title}</Card.Title>
+                                <Card.Text>{foods[2].content}</Card.Text>
+                                <Card.Text>{foods[2].price}</Card.Text>
+                                <Button variant="primary">상세보기</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
-            </Container>
+            </Container> */}
 
 
 
---
+++ react-study/src/foodmarket/components/FoodCard.jsx
@@ -0,0 +1,21 @@
+import { Card, Button } from 'react-bootstrap';
+
+// import food1 from '../img/food1.jpg';
+// import food2 from '../img/food2.jpg';
+// import food3 from '../img/food3.jpg';
+
+function FoodCard({ food, foods, index }) {
+    return (
+        <Card style={{ width: '18rem' }}>
+            {/* <Card.Img variant="top" src={'https://localhost:5173/' + food.imgPath} /> */}
+            <Card.Img variant="top" src={import.meta.env.BASE_URL + food.imgPath} />
+            <Card.Body>
+                <Card.Title>{foods[index].title}</Card.Title>
+                <Card.Text>{food.content}</Card.Text>
+                <Card.Text>{food.price}</Card.Text>
+                <Button variant="primary">상세보기</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+export default FoodCard;
